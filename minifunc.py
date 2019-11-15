3cca436cbed0169ca61fba901af5b1f1a1de5537
OlegF314 <o.l.e.g.f@yandex.ru>
2019-11-16 02:36:54 +0300

    Update minifunc.py

--- minifunc.py
+++ minifunc.py
@@ -46,7 +46,7 @@ def attack(n):
 def turn(n):
     global overload
     overload += 1
-    bots[botnum][3] = (bots[botnum][3] + n) % 8
+    bots[botnum][3] = n
     bots[botnum][4] = (bots[botnum][4] + 1) % 80
 
 
@@ -83,4 +83,45 @@ def move(n):
         bots[botnum][1] = x1
         canv.move(bots[botnum][5], movex[n] * SIZE, movey[n] * SIZE)
     bots[botnum][4] = (bots[botnum][4] + movegen[map1[y1][x1]]) % 80
-def back(n)
+def dead(bot):
+    global bots, alive, botnum
+    map1[bots[bot][0]][bots[bot][1]] = "0"
+    canv.create_rectangle(SIZE * bots[bot][1],
+                          SIZE * bots[bot][0],
+                          SIZE * (bots[bot][1] + 1),
+                          SIZE * (bots[bot][0] + 1),
+                          fill="lightblue")
+    bots = bots[:bot] + bots[bot + 1:] + bots[bot]
+    alive -= 1
+    botnum -= 1
+    if alive == 8:
+        mutate()
+
+
+def mutate():
+    global alive, turn_end, overload, botnum
+    alive = 64
+    turn_end = False
+    overload = 0
+    botnum = 1
+    for i in range(64):
+        genomes[i] = genomes[i % 8][:]
+        bots[i][2] = 20
+    for i in range(8, 64):
+        y = randint(0, 26)
+        x = randint(0, 47)
+        while map1[y][x] in ["*", "b"]:
+            y = randint(0, 26)
+            x = randint(0, 47)
+        map1[y][x] = "b"
+        canv.create_rectangle(SIZE * x,
+                              SIZE * y,
+                              SIZE * (x + 1),
+                              SIZE * (y + 1),
+                              fill="lightblue")
+        canv.move(bots[i][5], SIZE * (x - bots[i][1]), SIZE * (y - bots[i][0]))
+        bots[i][0] = y
+        bots[i][1] = x
+    for i in range(8):
+        for j in range(randint(1, 3)):
+            genomes[i][randint(0, 79)] = randint(0, 79)
