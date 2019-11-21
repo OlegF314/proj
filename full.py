aabed088f0a921fcfbda6eb393e899459c306bd3
OlegF314 <o.l.e.g.f@yandex.ru>
2019-11-21 23:04:50 +0300

    Update full.py

--- full.py
+++ full.py
@@ -26,28 +26,28 @@ movey = [-1, -1, 0, 1, 1, 1, 0, -1]
 movex = [0, 1, 1, 1, 0, -1, -1, -1]
 movegen = {"p" : 1, "*" : 2, "b" : 3, "f" : 4, "0" : 5}
 map1 = []
+map2 = []
 botnum = 1
 turn_end = False
 overload = 0
 gen_time = 0
+colors = {"*":"brown",
+          "0":"lightblue",
+          "p":"purple",
+          "f":"green",
+          "b":"red"}
 for i in range(HEIGHT):
     map1.append(list(list(MAP.readline())))
 for i in range(64):
     map1[10 + i % 8][20 + i // 8] = "b"
+for i in range(HEIGHT):
+    map2.append([canv.create_rectangle(j * SIZE, i * SIZE, (j + 1) * SIZE, (i + 1) * SIZE, fill=colors[map1[i][j]]) for j in range(WIDTH)])
+
 #0 = empty
 #* = wall
 #p = poison
 #f = food
 #b = bot
-def create_field():#20-27x10-17
-    for i in range(HEIGHT):
-        for j in range(WIDTH):
-            if map1[i][j] == '*':
-                canv.create_rectangle(j * SIZE, i * SIZE, (j + 1) * SIZE, (i + 1) * SIZE, fill="brown")
-            else:
-                canv.create_rectangle(j * SIZE, i * SIZE, (j + 1) * SIZE, (i + 1) * SIZE, fill="lightblue")
-    for i in range(64):
-        bots[i].append(canv.create_rectangle(SIZE * (20 + i // 8), SIZE * (10 + i % 8), SIZE * ((20 + i // 8) + 1), SIZE * ((10 + i % 8) + 1), fill="red"))
 
 def handover():
     global botnum, overload, turn_end, bots, genomes, gen_time
@@ -55,7 +55,7 @@ def handover():
     if bots[botnum][2] == 0:
         dead(botnum)
     if botnum == alive - 1:
-        botnum = 1
+        botnum = 0
         gen_time += 1
     else:
         botnum += 1
@@ -69,19 +69,11 @@ def grab(n):
     x1 = bots[botnum][1] + movex[n]
     if map1[y1][x1] == "p":
         map1[y1][x1] = "f"
-        canv.create_rectangle(SIZE * x1,
-                              SIZE * y1,
-                              SIZE * (x1 + 1),
-                              SIZE * (y1 + 1),
-                              fill="green")
+        canv.itemconfig(map2[y1][x1], fill="green")
     elif map1[y1][x1] == "f":
         map1[y1][x1] = "0"
         bots[botnum][2] += 10
-        canv.create_rectangle(SIZE * x1,
-                              SIZE * y1,
-                              SIZE * (x1 + 1),
-                              SIZE * (y1 + 1),
-                              fill="lightblue")
+        canv.itemconfig(map2[y1][x1], fill="lightblue")
     bots[botnum][4] = (bots[botnum][4] + movegen[map1[y1][x1]]) % 80
 
 
@@ -92,11 +84,7 @@ def attack(n):
     x1 = bots[botnum][1] + movex[n]
     if map1[y1][x1] in ["p", "f"]:
         map1[y1][x1] = "0"
-        canv.create_rectangle(SIZE * x1,
-                              SIZE * y1,
-                              SIZE * (x1 + 1),
-                              SIZE * (y1 + 1),
-                              fill="lightblue")
+        canv.itemconfig(map2[y1][x1], fill="lightblue")
     elif map1[y1][x1] == "b":
         for i in range(alive):
             if bots[i][0] == y1 and bots[i][1] == x1:
@@ -129,25 +117,23 @@ def move(n):
     x1 = bots[botnum][1] + movex[n]
     cell = map1[y1][x1]
     if cell == "f":
-        canv.create_rectangle(SIZE * x1,
-                              SIZE * y1,
-                              SIZE * (x1 + 1),
-                              SIZE * (y1 + 1),
-                              fill="lightblue")
+        canv.itemconfig(map2[y1][x1], fill="lightblue")
         bots[botnum][2] += 10
         map1[y1][x1] = "b"
         map1[bots[botnum][0]][bots[botnum][1]] = "0"
         bots[botnum][0] = y1
         bots[botnum][1] = x1
-        canv.move(bots[botnum][5], movex[n] * SIZE, movey[n] * SIZE)
+        canv.itemconfig(map2[bots[botnum][0]][bots[botnum][1]], fill="lightblue")
+        canv.itemconfig(map2[y1][x1], fill="red")
     elif cell == "p":
         dead(botnum)
     elif cell == "0":
         map1[y1][x1] = "b"
         map1[bots[botnum][0]][bots[botnum][1]] = "0"
+        canv.itemconfig(map2[y1][x1], fill="red")
+        canv.itemconfig(map2[bots[botnum][0]][bots[botnum][1]], fill="lightblue")
         bots[botnum][0] = y1
         bots[botnum][1] = x1
-        canv.move(bots[botnum][5], movex[n] * SIZE, movey[n] * SIZE)
     bots[botnum][4] = (bots[botnum][4] + movegen[map1[y1][x1]]) % 80
 
 
@@ -155,11 +141,7 @@ def move(n):
 def dead(bot):
     global bots, alive, botnum
     map1[bots[bot][0]][bots[bot][1]] = "0"
-    canv.create_rectangle(SIZE * bots[bot][1],
-                          SIZE * bots[bot][0],
-                          SIZE * (bots[bot][1] + 1),
-                          SIZE * (bots[bot][0] + 1),
-                          fill="lightblue")
+    canv.itemconfig(map2[bots[bot][0]][bots[bot][1]], fill="lightblue")
     bots = bots[:bot] + bots[bot + 1:] + [bots[bot]]
     alive -= 1
     botnum -= 1
@@ -174,7 +156,7 @@ def gen_food():
         m = randint(0,47)
         if map1[z][m] not in ["*", "b"]:
             map1[z][m] = "f"
-            canv.create_rectangle(m * SIZE, z * SIZE, (m + 1) * SIZE, (z + 1) * SIZE, fill="green")
+            canv.itemconfig(map2[z][m], fill="green")
             k += 1
     k = 0
     while k != 7 :
@@ -182,7 +164,7 @@ def gen_food():
         m = randint(0,47)
         if map1[z][m] not in ["*", "b"]:
             map1[z][m] = "p"
-            canv.create_rectangle(m * SIZE, z * SIZE, (m + 1) * SIZE, (z + 1) * SIZE, fill = "purple")
+            canv.itemconfig(map2[z][m], fill = "purple")
             k+=1
 
 
@@ -202,12 +184,7 @@ def mutate():
             y = randint(0, 26)
             x = randint(0, 47)
         map1[y][x] = "b"
-        canv.create_rectangle(SIZE * x,
-                              SIZE * y,
-                              SIZE * (x + 1),
-                              SIZE * (y + 1),
-                              fill="lightblue")
-        canv.move(bots[i][5], SIZE * (x - bots[i][1]), SIZE * (y - bots[i][0]))
+        canv.itemconfig(map2[y][x], fill="red")
         bots[i][0] = y
         bots[i][1] = x
     for i in range(8):
@@ -243,7 +220,5 @@ def mainfunc():
 
 
 
-
-create_field()
 mainfunc()
 mainloop()
