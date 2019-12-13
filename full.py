7e518cd7dd0266d2a5c9111353e9f005abdf68cf
OlegF314 <o.l.e.g.f@yandex.ru>
2019-12-13 22:16:42 +0300

    Update full.py

--- full.py
+++ full.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from random import *
+import sys
+from typing import List, Any
 
 WIDTH = 48
 HEIGHT = 27
@@ -11,47 +13,50 @@ canv = Canvas(root, width=1440, height=810, bg="black")
 canv.pack()
 genomes = [list(map(int, open("genom.evo", "r").readline().split())) for i in range(64)]
 bots = [[10 + i % 8, 20 + i // 8, 20, 0, 0] for i in range(64)]
-#[0] - y
-#[1] - x
-#[2] - HP
-#[3] - orientation:
-#7 0 1
-#6 b 2
-#5 4 3
-#[4] - genome pointer
-#[5] - graphic rectangle
+# [0] - y
+# [1] - x
+# [2] - HP
+# [3] - orientation:
+# 7 0 1
+# 6 b 2
+# 5 4 3
+# [4] - genome pointer
+# [5] - graphic rectangle
 alive = 64
 movey = [-1, -1, 0, 1, 1, 1, 0, -1]
 movex = [0, 1, 1, 1, 0, -1, -1, -1]
-movegen = {"p" : 1, "*" : 2, "b" : 3, "f" : 4, "0" : 5}
+movegen = {"p": 1, "*": 2, "b": 3, "f": 4, "0": 5}
 map1 = []
 map2 = []
 botnum = 0
 turn_end = False
 overload = 0
 gen_time = 0
-colors = {"*":"brown",
-          "0":"lightblue",
-          "p":"purple",
-          "f":"green",
-          "b":"red"}
+colors = {"*": "brown",
+          "0": "lightblue",
+          "p": "purple",
+          "f": "green",
+          "b": "red"}
 for i in range(HEIGHT):
-    map1.append(list(list(MAP.readline())))
+    map1.append(list(list(MAP.readline().strip())))
 for i in range(64):
     map1[10 + i % 8][20 + i // 8] = "b"
-print(*map1, sep="\n")
-for i in range(HEIGHT):
-    map2.append([canv.create_rectangle(j * SIZE, i * SIZE, (j + 1) * SIZE, (i + 1) * SIZE, fill=colors[map1[i][j]]) for j in range(WIDTH)])
 
-#0 = empty
-#* = wall
-#p = poison
-#f = food
-#b = bot
+for i in range(HEIGHT):
+    map2.append(
+        [canv.create_rectangle(j * SIZE, i * SIZE, (j + 1) * SIZE, (i + 1) * SIZE, fill=colors[map1[i][j]]) for j in
+         range(WIDTH)])
+
+
+# 0 = empty
+# * = wall
+# p = poison
+# f = food
+# b = bot
 
 def handover():
     global botnum, overload, turn_end, bots, genomes, gen_time
-    bots[botnum][2] -= 1        #Скушал хп у бота
+    bots[botnum][2] -= 1  # Скушал хп у бота
     if bots[botnum][2] <= 0:
         dead(botnum)
     if botnum == alive - 1:
@@ -61,6 +66,7 @@ def handover():
         botnum += 1
     overload = 0
     turn_end = False
+
 
 def grab(n):
     global turn_end, botnum
@@ -102,12 +108,10 @@ def turn(n):
     bots[botnum][4] = (bots[botnum][4] + 1) % 80
 
 
-
 def look(n):
     global overload, botnum
     overload += 1
     bots[botnum][4] = (bots[botnum][4] + movegen[map1[bots[botnum][0] + movey[n]][bots[botnum][1] + movex[n]]]) % 80
-
 
 
 def move(n):
@@ -125,8 +129,6 @@ def move(n):
         canv.itemconfig(map2[y1][x1], fill="red")
         bots[botnum][0] = y1
         bots[botnum][1] = x1
-    elif cell == "p":
-        dead(botnum)
     elif cell == "0":
         map1[y1][x1] = "b"
         map1[bots[botnum][0]][bots[botnum][1]] = "0"
@@ -134,15 +136,19 @@ def move(n):
         canv.itemconfig(map2[bots[botnum][0]][bots[botnum][1]], fill="lightblue")
         bots[botnum][0] = y1
         bots[botnum][1] = x1
+    elif cell == "p":
+        bots[botnum][2] = 0
+#        dead(botnum)
     bots[botnum][4] = (bots[botnum][4] + movegen[map1[y1][x1]]) % 80
 
 
-
 def dead(bot):
-    global bots, alive, botnum
+    global bots, alive, botnum, genomes
     map1[bots[bot][0]][bots[bot][1]] = "0"
     canv.itemconfig(map2[bots[bot][0]][bots[bot][1]], fill="lightblue")
+    bots[bot][0] = bots[bot][1] = -1
     bots = bots[:bot] + bots[bot + 1:] + [bots[bot]]
+    genomes = genomes[:bot] + genomes[bot + 1:] + [genomes[bot]]
     alive -= 1
     if botnum >= bot:
         botnum -= 1
@@ -152,29 +158,35 @@ def dead(bot):
 
 def gen_food():
     k = 0
-    while k != 2 :
-        z = randint(0,26)
-        m = randint(0,47)
+    while k != 2:
+        z = randint(0, 26)
+        m = randint(0, 47)
         if map1[z][m] not in ["*", "b"]:
             map1[z][m] = "f"
             canv.itemconfig(map2[z][m], fill="green")
             k += 1
     k = 0
-    while k != 3 :
-        z = randint(0,26)
-        m = randint(0,47)
+    while k != 3:
+        z = randint(0, 26)
+        m = randint(0, 47)
         if map1[z][m] not in ["*", "b"]:
             map1[z][m] = "p"
-            canv.itemconfig(map2[z][m], fill = "purple")
-            k+=1
+            canv.itemconfig(map2[z][m], fill="purple")
+            k += 1
 
 
 def mutate():
     global alive, turn_end, overload, botnum
+    #    print('\n'.join(map(''.join, map1)), end='\n\n')
+    print(sum(map1[i].count('b') for i in range(27)), alive)
     alive = 64
     turn_end = False
     overload = 0
-    botnum = 1
+    botnum = 0
+    qwer = sum([map1[i].count("b") for i in range(27)])
+    if alive < qwer:
+        print(*map1, sep="\n")
+        sys.exit()
     for i in range(64):
         genomes[i] = genomes[i % 8][:]
         bots[i][2] = 20
@@ -182,8 +194,8 @@ def mutate():
         y = randint(0, 26)
         x = randint(0, 47)
         while map1[y][x] in ["*", "b"]:
-            y = randint(0, 26)
-            x = randint(0, 47)
+            y = randint(1, 25)
+            x = randint(1, 46)
         map1[y][x] = "b"
         canv.itemconfig(map2[y][x], fill="red")
         bots[i][0] = y
@@ -191,38 +203,38 @@ def mutate():
     for i in range(8):
         for j in range(randint(1, 3)):
             genomes[i][randint(0, 79)] = randint(0, 79)
-#def step1():
 
+
+# def step1():
 
 
 def mainfunc():
-    global botnum, overload, turn_end,bots,genomes # Объявление глобальных переменных
+    global botnum, overload, turn_end, bots, genomes, alive  # Объявление глобальных переменных
     if botnum == 0:
-        gen_food()                     # Генерация еды
+        gen_food()  # Генерация еды
     flag = True
-    while genomes[botnum][bots[botnum][4]]>39:
+    while genomes[botnum][bots[botnum][4]] > 39:
         overload += 1
         bots[botnum][4] = (bots[botnum][4] + genomes[botnum][bots[botnum][4]]) % 80
         if overload == 10:
             flag = False
-            handover()       #Вызов функции передачи хода
+            handover()  # Вызов функции передачи хода
             break
     if flag:
         act = genomes[botnum][bots[botnum][4]] // 8
         n = genomes[botnum][bots[botnum][4]] % 8
         switcher = {  # Словарь который послужит переключателем команд
-        0: move,  # Тут написаны имена мини-функций
-        1: grab,
-        2: attack,
-        3: turn,
-        4: look
+            0: move,  # Тут написаны имена мини-функций
+            1: grab,
+            2: attack,
+            3: turn,
+            4: look
         }
         switcher[act](n)  # По ключу переходит к функции, аргументы функции задаются в ()
         if turn_end or overload == 10:  # Смена хода
             handover()
     root.after(1, mainfunc)
-
-
+print(*map1, sep="\n")
 
 mainfunc()
 mainloop()
